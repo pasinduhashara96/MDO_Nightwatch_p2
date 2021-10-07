01302c3de78955c51b67496c9d02a8a6f914347a
pasindu <pasinduhashara96bc@gmail.com>
2021-10-07 17:06:32 +0530

    My second branch commit

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,6 +1,9 @@
 module.exports = {
     'My first test case'(browser) {
-        
+        browser
+            .url('https://news.ycombinator.com/')
+            .waitForElementVisible('.hnname')
+            .assert.containsText(".hnname", "Hacker News")
 
         
     }
